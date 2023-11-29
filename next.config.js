d14c3cf19bffdc5675a9dbcb8835b2830368b897
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-11-29 04:29:21 +0100

    12er commit

--- next.config.js
+++ next.config.js
@@ -1,25 +1,36 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
   reactStrictMode: true,
+
   swcMinify: true,
+
   images: {
+
     domains: ["lh3.googleusercontent.com", "vercel.com"],
+
   },
+
   async redirects() {
+
     return [
+
       {
+
         source: "/github",
+
         destination: "https://github.com/steven-tey/precedent",
+
         permanent: false,
+
       },
-      
+
     ];
+
   },
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-};}
-}
+
+};
+
+
 module.exports = nextConfig;
